aa5e34d1010bd4f78c4d75684ec8152247368d29
wongm142 <wongm142@mcmaster.ca>
2025-03-13 16:32:58 -0400

    base drone class

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
@@ -0,0 +1,31 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+import java.io.StringReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import eu.ace_design.island.bot.IExplorerRaid;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class Drone {
+    private Direction currDirection;
+    private Battery batteryLevel;
+    private Coordinates currPosition;
+
+    public Drone (String direction, int initialBattery) {
+        batteryLevel = new Battery(initialBattery);
+        currDirection = Direction.valueOf(direction);
+        currPosition = new Coordinates(0,0,currDirection);
+    }
+    
+    public Direction getDirection(){
+        return currDirection;
+    }
+
+    public int getBattery(){
+        return batteryLevel.getBattery();
+    }
+
+    
+}
