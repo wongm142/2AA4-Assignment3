180de2147c53d7affb4539201d1da01bfcd39ee7
wongm142 <wongm142@mcmaster.ca>
2025-03-13 16:10:26 -0400

    coordinates system

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Coordinates.java
@@ -0,0 +1,55 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public class Coordinates {
+    private int xCoord;
+    private int yCoord;
+    private Direction direction;
+
+    public Coordinates(int xCoord, int yCoord, Direction direction){
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.direction = direction;
+    }
+
+    public int getXCoords(){
+        return xCoord;
+    }
+
+    public int getYCoords(){
+        return yCoord;
+    }
+
+    public Direction getDirection(){
+        return direction;
+    }
+
+    public void setDirection (Direction direction){
+        this.direction = direction;
+    }
+
+    public void flyForwards(){
+        switch (direction){
+            case N:
+                yCoord++;
+                break;
+            case S:
+                yCoord--;
+                break;
+            case E:
+                xCoord++;
+                break;
+            case W:
+                xCoord--;
+                break;
+        }
+    }
+
+    public void turnLeft(){
+        this.direction = direction.leftDirection();
+    }
+
+    public void turnRight(){
+        this.direction = direction.rightDirection();
+    }
+
+}
