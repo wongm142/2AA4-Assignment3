d08cf083f1b7675bf24ac98545d5165b0f5aa5c6
wongm142 <wongm142@mcmaster.ca>
2025-03-31 19:53:10 -0400

    deleted empty test case

--- src/test/java/ca/mcmaster/se2aa4/island/teamXXX/IslandFnderMoveToIslandTest.java
+++
@@ -1,9 +0,0 @@
-package ca.mcmaster.se2aa4.island.teamXXX;
-
-import org.json.JSONObject;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class IslandFnderMoveToIslandTest {
-
-}
